694535b4d1e7ee7abc17722281b791884055a916
Skylar <syema.ailia@gmail.com>
2016-08-20 22:40:48 -0500

    move files

--- numerology_app.rb
+++ numerology_app.rb
@@ -1,49 +1,40 @@
 require 'sinatra'
 require 'rspec'
 
+
 def enter_dob
 	puts "Please enter your date of birth in the following format: MMDDYYYY"
 	birthdate = gets.chomp
 	birthdate
 end
 
-def get_birthdate(number)
-	split_birthdate = number.split("")
+def get_birth_path_num(birthdate)
+    number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
-	number_1 = split_birthdate[0].to_i
-	number = 0
+    number = number.to_s
+    number = number[0].to_i + number[1].to_i
 
-	while split_birthdate.length > 1
-		number_1 = split_birthdate[0].to_i
-		number += number_1
-		split_birthdate.delete_at(0)
-	end
+    if number > 9
+        number = number.to_s
+        number = number[0].to_i + number[1].to_i
+    end
 
-	split_number = number.to_s.split("")
-	split_number_1 = split_number[0].to_i
-	final_number = 0
-
-	while split_number.length > 1
-		split_number_1 = split_number[0].to_i
-		final_number += split_number_1
-		split_number.delete_at(0)
-	end
-	final_number 
+    return number
 end
 
 def your_numerology(number)
 
 	case number
 		when 1
-			message = "Your numerology number is #{number}. One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
+			message = "One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
 		when 2
 			message = "This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
 		when 3
-			message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three’s enjoy life and have a good sense of humor. Ruled by Jupiter."
+			message = "Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
 		when 4
 			message = "This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
 		when 5
-			message = "This is the freedom lover. The number five is an intellectual vibration. These are ‘idea’ people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+			message = "This is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
 		when 6
 			message = "This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
 		when 7
@@ -59,7 +50,7 @@ end
 
 get '/:birthdate' do
 birthdate = params[:birthdate]
-birth_path_num = get_birthdate(birthdate)
+birth_path_num = get_birth_path_num(birthdate)
 message = your_numerology(birth_path_num)
 "#{message}"
 end
